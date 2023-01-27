a508a3ce6f1fdac7f3c61c3bfdce6c89eedfc143
davidlongshore <79716264+davidlongshore@users.noreply.github.com>
2023-01-26 19:54:08 -0800

    Add files via upload

---
+++ PRO-C158-Student-Activity-main/js/CursorEvents.js
@@ -0,0 +1,54 @@
+AFRAME.registerComponent("cursor-listener", {
+  schema: {
+    selectedItemId: { default: "", type: "string" },
+  },
+  init: function () {
+    this.handleMouseEnterEvents();
+    this.handleMouseLeaveEvents();
+    
+  },
+
+  handlePlacesListState: function () {
+    const id = this.el.getAttribute("id");
+    const placesId = ["taj-mahal", "budapest", "new-york-city", "eiffel-tower"];
+    if (placesId.includes(id)) {
+      const placeContainer = document.querySelector("#places-container");
+      placeContainer.setAttribute("cursor-listener", {
+        selectedItemId: id,
+      });
+      this.el.setAttribute("material", {
+        color: "#00ffff",
+        opacity: 1,
+      });
+    }
+  },
+
+  handlePlacesListState2: function () {
+    const id = this.el.getAttribute("id");
+    const placesId = ["taj-mahal", "budapest", "new-york-city", "eiffel-tower"];
+    if (placesId.includes(id)) {
+      const placeContainer = document.querySelector("#places-container");
+      placeContainer.setAttribute("cursor-listener", {
+        selectedItemId: id,
+      });
+      this.el.setAttribute("material", {
+        color: "#ff0000",
+        opacity: 1,
+      });
+    }
+  },
+
+  handleMouseEnterEvents: function () {
+    // Mouse Enter Events
+    this.el.addEventListener("mouseenter", () => {
+      this.handlePlacesListState();
+    });
+  },
+  handleMouseLeaveEvents: function () {
+    // Mouse Leave Events
+    this.el.addEventListener("mouseleave", () => {
+      this.handlePlacesListState2();
+
+    });
+  },
+});
